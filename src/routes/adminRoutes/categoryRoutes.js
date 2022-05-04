708a32a0b55468511fed1b51b0e8e00b93411c38
Vishamber <vishdas1111@gmail.com>
2022-05-04 23:21:02 +0530

    Added delete category route

--- src/routes/adminRoutes/categoryRoutes.js
+++ src/routes/adminRoutes/categoryRoutes.js
@@ -2,8 +2,15 @@ const express = require("express");
 const routes = express.Router();
 const { category } = require("../../controllers/index").adminCtrl;
 
+// category insert route
 routes.post("/insert", async (req, res) => {
 	let result = await category.insert(req);
+	return res.status(result.status).json(result);
+});
+
+// category delete route
+routes.delete("/delete/:id", async (req, res) => {
+	let result = await category.delete(req);
 	console.log({ result: result });
 	return res.status(result.status).json(result);
 });
