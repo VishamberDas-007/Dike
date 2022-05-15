1509854aba08c5dafe139e529325523246b328d6
Vishamber <vishdas1111@gmail.com>
2022-05-15 22:17:32 +0530

    Added product model

--- src/models/index.js
+++ src/models/index.js
@@ -19,18 +19,35 @@ db.role = require("./roleModel")(sequelize, Sequelize); // role model is importe
 db.user = require("./userModel")(sequelize, Sequelize); // user model is imported
 db.userHasRole = require("./userHasRoleModel")(sequelize, Sequelize); // userHasRole model is imported
 db.category = require("./categoryModel")(sequelize, Sequelize); // category model is imported
+db.product = require("./productModel")(sequelize, Sequelize); // product model is imported
 
 // relations between the models to be defined here
 
+// userHasRole and role table relation
 db.userHasRole.belongsTo(db.role, {
 	foreignKey: "roleID",
 	onUpdate: "CASCADE",
 	onDelete: "CASCADE",
 });
 
+// user and userHasRole table relation
 db.user.hasOne(db.userHasRole, {
 	foreignKey: "userID",
 	onUpdate: "CASCADE",
 	onDelete: "CASCADE",
 });
+
+// product and category table relations
+db.product.belongsTo(db.category, {
+	foreignKey: "catID",
+	onUpdate: "CASCADE",
+	onDelete: "CASCADE",
+});
+
+db.category.hasMany(db.product, {
+	foreignKey: "catID",
+	onUpdate: "CASCADE",
+	onDelete: "CASCADE",
+});
+
 module.exports = db; // db is exported
