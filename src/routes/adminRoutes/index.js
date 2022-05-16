b77d98fee0a2c9b6bc5308599c46b60a35d5964d
Vishamber <vishdas1111@gmail.com>
2022-05-16 21:53:41 +0530

    Indexed the product routes

--- src/routes/adminRoutes/index.js
+++ src/routes/adminRoutes/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const routes = express.Router();
 const verify = require("../../middlewares/verify");
 
-routes.use("/auth", require("./authRoutes"));
-routes.use("/category", verify.isAdmin, require("./categoryRoutes"));
+routes.use("/auth", require("./authRoutes")); // all auth routes are indexed here
+routes.use("/category", verify.isAdmin, require("./categoryRoutes")); // all category routes are indexed here
+routes.use("/product", require("./productRoutes")); // all product routes are indexed here
 
 module.exports = routes;
