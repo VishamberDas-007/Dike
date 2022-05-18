9391826b286bb9cb8e14cfa917a01e39ad6550e2
Vishamber <vishdas1111@gmail.com>
2022-05-18 16:34:56 +0530

    Added the multer for uploading multiple images

--- src/routes/adminRoutes/productRoutes.js
+++ src/routes/adminRoutes/productRoutes.js
@@ -4,29 +4,45 @@ const { product } = require("../../controllers/index").adminCtrl;
 const multer = require("multer");
 const path = require("path");
 const fileLocation = path.join(__dirname, "../../uploads");
-// const services = require("../../services/services");
-// const upload = services.upload;
 
 var fileName, timeStamp, fileExtension, tempFileName;
+var array = [],
+	j = 0;
 
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, fileLocation);
+		fileExtension = path.extname(file.originalname);
+
+		// validating the extension
+		if (
+			fileExtension == ".jpg" ||
+			fileExtension == ".png" ||
+			fileExtension == ".jpeg" ||
+			fileExtension == ".gif"
+		)
+			cb(null, fileLocation);
+		else cb("Upload Image only");
 	},
+
 	filename: function (req, file, cb) {
 		timeStamp = Date.now();
 		fileExtension = path.extname(file.originalname);
 		fileName = timeStamp.toString() + fileExtension;
 		tempFileName = fileName;
+		array[j] = fileName;
+		j++;
 		cb(null, fileName);
 	},
 });
 var upload = multer({ storage: storage });
 
-// product insert route
-routes.post("/insert", upload.single("file"), async (req, res) => {
-	console.log({ fileExtension: fileExtension });
-	let result = await product.insert(req, fileName, fileExtension);
+// product insert route for multiple image upload max count 5
+
+routes.post("/insert", upload.array("file", 5), async (req, res) => {
+	let result = await product.insert(req, array);
+	console.log({ result: result });
+	j = 0;
+	array = [];
 	return res.status(result.status).json(result);
 });
 
